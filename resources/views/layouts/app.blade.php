0fb486380f1e8a6d4fbb6808e592924f486d7498
unknown <usuario@BM0QTFGB.torax.cl>
2024-09-10 14:56:33 -0300

    inicio proyecto

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite([
+                'resources/css/app.scss',
+                'resources/js/app.js',
+                ])
+
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="" id="app">
+            <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark shadow-sm">
+                <div class="container">
+                    <div>
+                        <a href="" class="navbar-brand">
+                            hola
+                        </a>
+                    </div>
+                </div>
+            </nav>
+            
+        </div>
+        
+    </body>
+</html>
